5592d7f5d1a78bca56a9edf44c83e109b3134cca
unluckyvevo <unluckyvevo@gmail.com>
2020-04-01 19:38:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-#from MapAPI import Map
+from MapAPI import Map
+import numpy as np
+import osmnx as ox
+import networkx as nx
+from sklearn.neighbors import KDTree
+import matplotlib.pyplot as plt
+import overpy as opy
+import Djistra as dj
 
-#print("test")
-#m = Map()
-#m.loadfromOSM("map2.osm")
-#node = m.getNearestNodeByCoord(1.4020051, 103.9060055, 20)
-from Routing import perform
-perform(1.4017320, 103.9076350, 1.3990941, 103.9076522)
+
+
+#Map.getNearestNodeByCoord(self,)
+
+
+
+
+class mapCall:
+    def getRoute(self,src,dst):
+        
+
+        
+        orgin_point=(float(src[0]),float(src[1]))
+        end_point=(float(dst[0]),float(dst[1]))
+        
+        k=ox.core.graph_from_file(r"C:\Users\User\.spyder-py3\data\Punggol.osm")
+        sh=dj.perform(float(src[0]),float(src[1]),float(dst[0]),float(dst[1]))
+        ox.plot_graph_route(k, sh, fig_height=10, fig_width=10,save=True,filename='PunggolPath',file_format='png')
+        return 'PunggolPath.png'
+#from Routing import perform
+#perform(1.4017320, 103.9076350, 1.3990941, 103.9076522)
