a506449e3bf5503449538bd5880de053300e51b8
unluckyvevo <unluckyvevo@gmail.com>
2020-04-03 22:34:49 +0800

    Update tkGUI.py
    Added a tab to get the top 10 closest restaurants and their coordinates nearby

--- tkGUI.py
+++ tkGUI.py
@@ -3,7 +3,6 @@ import numpy as np
 import osmnx as ox
 import networkx as nx
 from sklearn.neighbors import KDTree
-import folium
 import matplotlib.pyplot as plt
 import overpy as oy
 from main import mapCall as mc
@@ -16,85 +15,102 @@ from main import mapCall as mc
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(659, 646)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(140, 250, 75, 23))
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setGeometry(QtCore.QRect(0, 0, 741, 641))
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.PointABox = QtWidgets.QLineEdit(self.tab)
+        self.PointABox.setGeometry(QtCore.QRect(120, 70, 261, 20))
+        self.PointABox.setObjectName("PointABox")
+        self.label = QtWidgets.QLabel(self.tab)
+        self.label.setGeometry(QtCore.QRect(230, 40, 47, 13))
+        self.label.setObjectName("label")
+        self.lbl_EnterCoordinates = QtWidgets.QLabel(self.tab)
+        self.lbl_EnterCoordinates.setGeometry(QtCore.QRect(210, 10, 241, 20))
+        self.lbl_EnterCoordinates.setObjectName("lbl_EnterCoordinates")
+        self.PointBBox = QtWidgets.QLineEdit(self.tab)
+        self.PointBBox.setGeometry(QtCore.QRect(120, 140, 261, 20))
+        self.PointBBox.setObjectName("PointBBox")
+        self.label_2 = QtWidgets.QLabel(self.tab)
+        self.label_2.setGeometry(QtCore.QRect(240, 110, 61, 16))
+        self.label_2.setObjectName("label_2")
+        self.checkBox = QtWidgets.QCheckBox(self.tab)
+        self.checkBox.setGeometry(QtCore.QRect(100, 170, 121, 31))
+        self.checkBox.setObjectName("checkBox")
+        self.pushButton = QtWidgets.QPushButton(self.tab)
+        self.pushButton.setGeometry(QtCore.QRect(120, 210, 75, 23))
         self.pushButton.setObjectName("pushButton")
         self.pushButton.clicked.connect(self.on_click)
-        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox.setGeometry(QtCore.QRect(140, 220, 121, 31))
-        self.checkBox.setObjectName("checkBox")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(250, 80, 47, 13))
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(250, 150, 61, 16))
-        self.label_2.setObjectName("label_2")
-        self.PointABox = QtWidgets.QLineEdit(self.centralwidget)
-        self.PointABox.setGeometry(QtCore.QRect(140, 100, 261, 20))
-        self.PointABox.setObjectName("PointABox")
-        self.PointBBox = QtWidgets.QLineEdit(self.centralwidget)
-        self.PointBBox.setGeometry(QtCore.QRect(140, 180, 261, 20))
-        self.PointBBox.setObjectName("PointBBox")
-        self.lbl_EnterCoordinates = QtWidgets.QLabel(self.centralwidget)
-        self.lbl_EnterCoordinates.setGeometry(QtCore.QRect(170, 20, 281, 20))
-        self.lbl_EnterCoordinates.setObjectName("lbl_EnterCoordinates")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(50, 300, 581, 271))
-        self.label_3.setMinimumSize(QtCore.QSize(0, 271))
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_POI = QtWidgets.QWidget()
+        self.tab_POI.setObjectName("tab_POI")
+        self.tableWidget = QtWidgets.QTableWidget(self.tab_POI)
+        self.tableWidget.setGeometry(QtCore.QRect(50, 110, 501, 321))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setRowCount(10)
+        self.tableWidget.cellClicked()
+        self.label_3 = QtWidgets.QLabel(self.tab_POI)
+        self.label_3.setGeometry(QtCore.QRect(190, 10, 181, 20))
         self.label_3.setObjectName("label_3")
+        self.SecondTabTextBox = QtWidgets.QLineEdit(self.tab_POI)
+        self.SecondTabTextBox.setGeometry(QtCore.QRect(140, 40, 261, 20))
+        self.SecondTabTextBox.setObjectName("SecondTabTextBox")
+        self.pushButton_2 = QtWidgets.QPushButton(self.tab_POI)
+        self.pushButton_2.setGeometry(QtCore.QRect(220, 70, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.clicked.connect(self.getTable)
+        self.tabWidget.addTab(self.tab_POI, "")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 659, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
         self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "ShortestPath "))
-        self.pushButton.setText(_translate("MainWindow", "OK"))
-        self.checkBox.setText(_translate("MainWindow", "Use sheltered Path"))
         self.label.setText(_translate("MainWindow", "Point A"))
+        self.lbl_EnterCoordinates.setText(_translate("MainWindow", "Enter the coordinates "))
         self.label_2.setText(_translate("MainWindow", "Point B"))
-        self.lbl_EnterCoordinates.setText(_translate("MainWindow", "Enter the coordinates in this format (Lat,long)"))
-         
-        
-        pixmap = QtGui.QPixmap()
-        pixmap.load('C:/Users/User/.spyder-py3/images/{}'.format("PunggolDefault.png"))
-        pixmap = pixmap.scaledToHeight(400)
-        self.label_3.setPixmap(pixmap)
-        self.label_3.show()
-        
-        #self.label_3.setText(_translate("MainWindow", "<html><head/><body><p><img src=\"C:/Users/User/.spyder-py3/images/PunggolPath.png\"/></p></body></html>"))
+        self.checkBox.setText(_translate("MainWindow", "Use sheltered Path"))
+        self.pushButton.setText(_translate("MainWindow", "OK"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Get By Coordinates"))
+        self.label_3.setText(_translate("MainWindow", "Enter your coordinates here"))
+        self.pushButton_2.setText(_translate("MainWindow", "Ok"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_POI), _translate("MainWindow", "Go to Point of Interest"))
+      
 
     def on_click(self):
         _translate = QtCore.QCoreApplication.translate
         sA = self.PointABox.text().split(",")
         dA=self.PointBBox.text().split(",")
-        checkBoxValue=self.checkBox.isChecked();
+        #checkBoxValue=self.checkBox.isChecked();
         D=mc()
-        pic=D.getRoute(sA,dA)
-        print(pic)
-       
+        D.getRoute(sA[0],sA[1],dA[0],dA[1])     
         
-        pixmap = QtGui.QPixmap()
-        pixmap.load('C:/Users/User/.spyder-py3/images/{}'.format(pic))
-        pixmap = pixmap.scaledToHeight(400)
-       # self.label_3.setText(_translate("MainWindow", imgsrc))
-        self.label_3.setPixmap(pixmap)
-        self.label_3.show()
+        
+    def getTable(self):
+         _translate = QtCore.QCoreApplication.translate
+         queryRes=self.SecondTabTextBox.text().split(",")
+         queryTable = ox.pois_from_point((float(queryRes[0]),float(queryRes[1])),distance=3000,amenities=['restaurant'])
+         cols=['name','geometry']
+         counter=0
+         for i in range(10):
+             self.tableWidget.setItem(i,0, QtWidgets.QTableWidgetItem(str(queryTable[cols].head(10).values.item(counter))))
+             counter+=1
+             self.tableWidget.setItem(i,1, QtWidgets.QTableWidgetItem(str(queryTable[cols].head(10).values.item(counter))))
+             counter+=1
+       
+         
         
         
 if __name__ == "__main__":
