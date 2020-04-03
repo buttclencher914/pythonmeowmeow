af7c9b99c2a74e1936503cedf83ec38d00515154
unluckyvevo <unluckyvevo@gmail.com>
2020-04-03 22:56:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import networkx as nx
 from sklearn.neighbors import KDTree
 import matplotlib.pyplot as plt
 import overpy as opy
-import Routing as rt
-import DisplayRoute as dr
+import Djistra as rt
+import over as dr
 
 
 # Map.getNearestNodeByCoord(self,)
@@ -14,16 +14,12 @@ import DisplayRoute as dr
 
 class mapCall:
     def getRoute(self, x1, y1, x2, y2):
-        k = ox.core.graph_from_file(r"C:\Users\Ian\Documents\GitHub\pythonmeowmeow\Punggol.osm")
-        sh = rt.perform(x1, y1, x2, y2)
-        ox.plot_graph_route(k, sh, fig_height=10, fig_width=10, save=True, filename='PunggolPath', file_format='png')
-        return 'PunggolPath.png'
+        #k = ox.core.graph_from_file(r"C:\Users\User\.spyder-py3\data\map2.osm")
+        dat = rt.perform(float(x1), float(y1), float(x2), float(y2))
+        dis = dr.DisplayRoute()
+        dis.display(dat[0], dat[1])
+        
+   
 
 
-# ff = mapCall()
-# ff.getRoute(1.4014434, 103.9082113, 1.3997726, 103.9116292)
-dat = rt.perform(1.4014434, 103.9082113, 1.3997726, 103.9116292)
-dis = dr.DisplayRoute()
-dis.display(dat)
-# from Routing import perform
-# perform(1.4017320, 103.9076350, 1.3990941, 103.9076522)
+
