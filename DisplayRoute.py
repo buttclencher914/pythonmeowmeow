88d58a2c77adb6fb6255f69d8c31986af6035053
unluckyvevo <unluckyvevo@gmail.com>
2020-04-04 14:26:51 +0800

    Update DisplayRoute.py

--- DisplayRoute.py
+++ DisplayRoute.py
@@ -1,7 +1,9 @@
 import webbrowser
-
-
+import Routing as rt
 class DisplayRoute:
+    
+    
+    
     def display(self, nodelist):
         url = "http://overpass-turbo.eu/map.html?Q=node(id%3A"
         for n in nodelist:
@@ -9,3 +11,8 @@ class DisplayRoute:
         url = url[:-3]
         url += ")%3Bway%5Bhighway%5D(bn)%3Bout%20geom%3B"
         webbrowser.open(url, new=0, autoraise=True)
+class mapCall:
+    def getRoute(self, x1, y1, x2, y2,m):
+      
+        dat = rt.perform(float(x1), float(y1), float(x2), float(y2),m)
+        DisplayRoute.display(self,dat)
